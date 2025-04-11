de089ecde3fb7bf3a776aa0b3b3c4cae5d6c66bd
Ana <ana.furlaneto@aluno.senai.br>
2025-04-11 14:08:45 -0300

    teste

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ func sayGreeting(nome string){
 }
 func addNumber(numero1 int, numero2 int)int{
     return numero1 + numero2
-}
+
+    func versaldo(){
+        fmt.Println("seu saldo é:")
+    }
+    func depositar(){
+        fmt.Println("deposite um valor:")
+    }
+    func sacar(){
+        fmt.Println("saque um valor:")
+    }
+    
+    fmt.Print("\nDigite "sacar", "depositar", "ver saldo" ou "sair": ")
+    fmt.Scan(&acao)
+    
+    }
+
 func main() {
     sayGreeting("Juvelino")
     resultado := addNumber(10, 20)
