cc105e1e37dcb84df83961d5d6833037e43dfe8e
Ana <ana.furlaneto@aluno.senai.br>
2025-04-24 15:23:40 -0300

    estoquecantina

--- main.go
+++ main.go
@@ -1,21 +1,19 @@
 package main
 
-import "fmt"
+import "fmt" 
 
-func analisarNotas(nota1, nota2 float64) (float64, string) {
-	media := (nota1 + nota2) / 2
-	if media >= 6 {
-		return media, "Aprovado"
-	} else {
-		return media, "Reprovado"
+func main() { 
+	estoque := make(map[string]int) 
+	estoque["Coxinha"] = 20
+	estoque["Pão de Queijo"] = 10
+estoque["Refrigerante"] = 20
+
+		estoque ["Coxinha"] -= 2
+		estoque ["Pão de Queijo"] -=1
+	
+	for produto, total := range estoque {
+	fmt.Printf("%s : Produto com %d de estoque\n", produto, total) 
 	}
+	} 
 
-	}
-
-
-func main(){
-	nota1 := 7.5
-	nota2 := 5.5
-	media, resultado := analisarNotas(nota1, nota2)
-	fmt.Printf("A média é: %.2f e o resultado é: %s\n", media, resultado)
-}
+	
